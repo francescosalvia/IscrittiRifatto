5ac1e808fd098083efecceafb51ef88179b03e46
Francesco Salvia <francesco.salvia@ext.contactlab.com>
2020-03-11 15:35:01 +0100

    Aggiunto config

---
+++ src/main/java/com/contactlab/iscritti/config/UtentiConfig.java
@@ -0,0 +1,21 @@
+package com.contactlab.iscritti.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@ComponentScan(basePackages = "com.contactlab.iscritti")
+public class UtentiConfig {
+
+    @Bean
+    public ThreadPoolTaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor pool = new ThreadPoolTaskExecutor();
+        pool.setCorePoolSize(10);
+        pool.setMaxPoolSize(20);
+        pool.setWaitForTasksToCompleteOnShutdown(true);
+        return pool;
+    }
+
+}
